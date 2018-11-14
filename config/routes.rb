ce9180a74cb8e2fb66de0f84ca04b24806286f92
Santi <pellegrino.santi@gmail.com>
2018-11-13 21:06:13 -0300

    puja

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :auctions
   resources :residences
   resources :users
-  get '/auctions/:id', to: 'auctions#show'
+  post '/auctions/:id', to: 'auctions#show'
 end
