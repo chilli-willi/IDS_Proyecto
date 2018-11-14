20e061d3d5bccd40cc022283e3f575d5b7f9c7ac
chilli-willi <gali7.jag@gmail.com>
2018-11-14 10:31:24 -0300

    Revert "Merge branch 'master' of https://github.com/chilli-willi/IDS_Proyecto"
    This reverts commit e60313865178b0f36e682066e71bcd70322a0cb6, reversing
    changes made to 084cebe04297dcbb56f196417f496689251b6582.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :auctions
   resources :residences
   resources :users
-  resources :home
-  
   post '/auctions/:id', to: 'auctions#show'
   post '/residences/:id', to: 'residences#show'
 
