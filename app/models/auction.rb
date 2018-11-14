c4fa5df98fd4cddfead4e3dbe30fc6fdd19b2023
chilli-willi <gali7.jag@gmail.com>
2018-11-14 10:20:53 -0300

    A

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,8 +1,8 @@
 class Auction < ApplicationRecord
-	validates :name, presence: true
-	validates :des, presence: true
-	validates :monto, presence: true
-	validates :minimapuja, presence: true
+	#validates :name, presence: true
+	#validates :des, presence: true
+	#validates :monto, presence: true
+	#validates :minimapuja, presence: true
 
 	has_many :residences
 	#belongs_to:residence
