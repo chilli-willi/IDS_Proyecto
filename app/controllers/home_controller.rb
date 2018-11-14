d694fece75e0998aeddfa6afe1f792023042e7b4
Santi <pellegrino.santi@gmail.com>
2018-11-14 11:21:19 -0300

    home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 	def index
-		redirect_to home_path
+		
 	end
+
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :auctions
   resources :residences
   resources :users
+  resources :home
   post '/auctions/:id', to: 'auctions#show'
   post '/residences/:id', to: 'residences#show'
-
+  root :to => "home#index"
 end
