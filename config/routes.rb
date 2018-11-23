6fbd52dd4396c119ae8fcf550807fcfd8c763386
BigBurrito007 <basantacristian@gmail.com>
2018-11-23 15:29:17 -0300

    borre modelo admin no era necesario

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  #devise_for :users
+ devise_for :users
   resources :auctions
   resources :residences
   resources :home
-  devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+  #devise_for :users, controllers: {
+  #      sessions: 'users/sessions'
+  #    }
   post '/auctions/:id', to: 'auctions#show'
   post '/residences/:id', to: 'residences#show'
   root :to => "home#index"
--- db/schema.rb
+++ db/schema.rb
@@ -41,18 +41,6 @@ ActiveRecord::Schema.define(version: 2018_11_21_023029) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
   create_table "residences", force: :cascade do |t|
     t.string "name"
     t.string "des"
