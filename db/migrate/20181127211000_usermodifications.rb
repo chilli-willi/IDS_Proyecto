f41d3276f7667c58b389f8c335123a03bc8d086f
Santi <pellegrino.santi@gmail.com>
2018-11-27 18:39:32 -0300

    Usermodification

---
+++ db/migrate/20181127211000_usermodifications.rb
@@ -0,0 +1,8 @@
+class Usermodifications < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :users, :age, :string
+  	add_column :users, :card, :integer
+  	add_column :users, :cvv, :integer
+  	add_column :users, :exp, :date
+  end
+end
