e96f281a60a1445b3a62ff4ded704c6a74cf5c62
BigBurrito007 <basantacristian@gmail.com>
2018-12-10 18:52:41 -0300

    solo se puede reservar con 6 meses de anticipacion

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,17 @@
 class Reservation < ApplicationRecord
 	
 	validates :weekdate, uniqueness: true
-	
+	validate :validate_week
+
+  private
+
+  def validate_week
+  	  date = DateTime.parse(weekdate)
+      if date.present? && date < Date.today + 6.month
+          errors.add(:weekdate, 'Debes reservar con 6 meses de anticipacion')
+      end
+  end
+
 	enum modo: { reserva: 0, subasta: 1, hotsale: 2 }
 	 
 	belongs_to :user
