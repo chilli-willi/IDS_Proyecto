5d3eac8f0dad9823233c23ec2531a36d4439ef4a
BigBurrito007 <basantacristian@gmail.com>
2018-12-14 14:18:57 -0300

    nuevo buscador

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -5,23 +5,30 @@ class ResidencesController < ApplicationController
 	end
 
 	def index
-		@residences = Residence.all
-	  	@tasks = if !params[:weekini].blank? and !params[:weekfin].blank?
-	      			@NoPueden = Reservation.all.where('weekdate BETWEEN ? AND ?', "#{params[:weekini]}", "#{params[:weekfin]}").select('residence_id').collect(&:residence_id)
-					@Sipueden = Residence.all.where('id NOT IN (?)', Array.wrap(@NoPueden))
-					@residences = @Sipueden	
-				#else 
-				#	flash[:notice] = 'Inicial menor de 6 meses' end
-				end
+  @residences = Residence.all
+    @tasks = if !params[:weekini].blank? and !params[:weekfin].blank?
+          
+          @NoPueden = Reservation
+            .where('weekdate BETWEEN ? AND ?', "#{params[:weekini]}", "#{params[:weekfin]}")
+            .pluck(:residence_id)
+     
 
-				if !params[:term].blank?
-					if @Sipueden != nil
-						@residences = @Sipueden.where('name ILIKE ? OR pais ILIKE ? OR localidad ILIKE ? OR provincia ILIKE ? OR des ILIKE ? ', "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%")
-	  				else 
-	  					@residences = Residence.all.where('name ILIKE ? OR pais ILIKE ? OR localidad ILIKE ? OR provincia ILIKE ? OR des ILIKE ? ', "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%")
-	  				end
-	   			end	   
-	end
+
+     #@Sipueden = Residence.all.where.('id NOT IN (?)', Array.wrap(@NoPueden))
+     @Sipueden = Residence.where.not(id: @NoPueden)
+     @residences = @Sipueden 
+    #else 
+    # flash[:notice] = 'Inicial menor de 6 meses' end
+    end
+
+    if !params[:term].blank?
+     if @Sipueden != nil
+      @residences = @Sipueden.where('name ILIKE ? OR pais ILIKE ? OR localidad ILIKE ? OR provincia ILIKE ? OR des ILIKE ? ', "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%")
+       else 
+        @residences = Residence.all.where('name ILIKE ? OR pais ILIKE ? OR localidad ILIKE ? OR provincia ILIKE ? OR des ILIKE ? ', "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%")
+       end
+       end    
+ end
 
 	def create
 		@residence = Residence.new(params.require(:residence).permit(:name, :des, :location, :pais, :provincia, :localidad))
